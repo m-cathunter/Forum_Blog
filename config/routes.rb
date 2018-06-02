37ec5757c4f47d8a2a1db7fe28007dcbbcbaef4e
m-cathunter <m.kotolowska@gmail.com>
2018-06-02 21:21:53 +0200

    Change rooting method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'test/index'
+#  get 'test/index'
+  root 'test#index'
+  match ':controller(/:action(/:id))', :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
